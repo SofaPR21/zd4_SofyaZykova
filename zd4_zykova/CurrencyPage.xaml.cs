a6cf573fd7866c2be24002a22d88531d15d933f9
Sofya <he.october1510@gmail.com>
2025-06-14 22:50:32 +0500

    Initial commit

---
+++ zd4_zykova/CurrencyPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd4_zykova
+{
+    public partial class CurrencyPage : ContentPage
+    {
+        public CurrencyPage()
+        {
+            InitializeComponent();
+            LoadRates();
+        }
+
+        private void LoadRates()
+        {
+            dateLabel.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            usdLabel.Text = "75.50 ₽";
+            eurLabel.Text = "85.30 ₽";
+        }
+
+        private void OnUpdateRatesClicked(object sender, EventArgs e)
+        {
+            LoadRates();
+            DisplayAlert("Обновлено", "Курсы валют обновлены", "OK");
+        }
+    }
+}
